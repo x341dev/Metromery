5f5168dab35a778a5a2fad1f5caf41852a826ef8
x341dev <admin@x341.dev>
2026-04-17 09:29:37 +0200

    feat: add CardsScreen component with grid layout for card display

---
+++ composeApp/src/commonMain/kotlin/dev/x341/metromery/screen/CardsScreen.kt
@@ -0,0 +1,38 @@
+package dev.x341.metromery.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.x341.metromery.MetromeryViewModel
+import dev.x341.metromery.component.CardComponent
+
+@Composable
+fun CardsScreen(
+    viewModel: MetromeryViewModel
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(viewModel.cards) { card ->
+                CardComponent(card = card, initialFlipped = false)
+            }
+        }
+    }
+}
