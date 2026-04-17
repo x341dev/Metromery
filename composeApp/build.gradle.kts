8c251602b0601703a51b8efaa2c80f2d0e8efd2c
x341dev <admin@x341.dev>
2026-04-17 10:41:59 +0200

    chore: add material icons extended dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,6 +50,7 @@ kotlin {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
+            implementation(libs.material.icons.extended)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
